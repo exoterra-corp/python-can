345c3b7eeac64503919df18821c1645d4b4dbc9e
Jeremy Mitchell <jmitchell@exoterracorp.com>
2024-07-03 12:17:40 -0600

    JTM - fixed readme bug

--- can/interfaces/exoserial/__init__.py
+++ can/interfaces/exoserial/__init__.py
@@ -2,4 +2,5 @@
 """
 
 from can.interfaces.exoserial.exoserial_can import ExoSerialBus as Bus
+from can.interfaces.exosocket.exosocket_can import ExoSocketBus as ExoSocketBus
 from .receiver import *
